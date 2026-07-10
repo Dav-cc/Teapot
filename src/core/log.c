e5f87fa4c95e953a35d450fb263826d7510262f6
Davoud Ghafouri <ghafooridavood580@gmail.com>
2026-07-10 17:21:11 +0330

    feat(logging): implement logging setup

---
+++ src/core/log.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+
+typedef enum {
+    LOG_LEVEL_DEBUG = 0,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_ERROR
+} LLevel;
+
+LLevel log_level = LOG_LEVEL_DEBUG;
+
+void log_message(LLevel level, char* format, ...) {
+    if (log_level > level) {
+        return;
+    }
+
+    va_list args;
+    va_start(args, format);
+
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    char time_str[20];
+    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", t);
+
+    printf("[%s] - ", time_str);
+    vprintf(format, args);
+    printf("\n");
+
+    va_end(args);
+}
+
